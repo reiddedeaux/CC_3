b0c4240bde5ca3510621fcf2913548b22c150d24
reiddedeaux <rdedeaux@usf.edu>
2025-02-02 20:25:55 -0500

    Task 1 - Product Price Management

---
+++ CC_3.js
@@ -0,0 +1,7 @@
+//Task 1 Product Price Management
+
+
+let prices = [10.99, 11.99, 12.99, 13.99, 14.99] ; // Declaring an array with 5 numerical values as prices
+prices.push(15.99) ; //Adding a new price into the array
+prices.shift(); // Getting rid of the first prive from the array
+console.log("New Prices:", prices); //Logging the new array into the console
